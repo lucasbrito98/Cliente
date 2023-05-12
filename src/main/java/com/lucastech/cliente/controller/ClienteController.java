c0725d364275d34ad6007a0bc769468e92ac7e63
LUCAS <lucaslima897@gmail.com>
2023-05-11 22:55:49 -0400

    adicionando pacotes service e implementando metodos

--- src/main/java/com/lucastech/cliente/controller/ClienteController.java
+++ src/main/java/com/lucastech/cliente/controller/ClienteController.java
@@ -1,4 +1,20 @@
 package com.lucastech.cliente.controller;
 
+import com.lucastech.cliente.entities.Cliente;
+import com.lucastech.cliente.service.ClienteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("v1/clientes")
 public class ClienteController {
+    private final ClienteService clienteService;
+
+@PostMapping
+@ResponseStatus(HttpStatus.CREATED)
+    public Cliente criar (@RequestBody Cliente clienteNovo){
+        return clienteService.criar(clienteNovo);
+    }
 }
